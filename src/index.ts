b8e013eb1af1aa6ec76d37c684616dc2b64b70b9
Albert Sevy <albertstorres@gmail.com>
2024-01-25 22:13:56 -0300

    Iniciado o repositório da aula de tipos de dados

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+let nome: string = "Anthony";//EXEMPLO DE TIPAGEM EXPLÍCITA. STRING
+
+console.log(nome);
+console.log(typeof nome);//RETORNA O TIPO DA VARIÁVEL NOME
+
+let sobrenome: any;//TIPAGEM EXPLÍCITA. ANY
+let maiorIdade = true;
+
+sobrenome = "Sevy";//STRING
+sobrenome = 3;//NUMBER
+
+console.log(typeof sobrenome);
